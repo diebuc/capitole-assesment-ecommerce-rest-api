e60787291572e2dfaebcc34d208245f988da0870
Diego B. Rey <diebuc@gmail.com>
2021-02-22 17:16:05 +0100

    H2 config params and data seed

---
+++ src/main/resources/data.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS prices;
+
+CREATE TABLE PRICES (
+                            PRICE_ID LONG AUTO_INCREMENT PRIMARY KEY,
+                            BRAND_ID INT,
+                            START_DATE TIMESTAMP NOT NULL,
+                            END_DATE TIMESTAMP NOT NULL,
+                            PRICE_LIST INT NOT NULL,
+                            PRODUCT_ID LONG NOT NULL,
+                            PRIORITY SMALLINT NOT NULL,
+                            PRICE DOUBLE NOT NULL,
+                            CURR VARCHAR(3) NOT NULL
+);
+
+INSERT INTO PRICES (BRAND_ID, START_DATE, END_DATE, PRICE_LIST, PRODUCT_ID, PRIORITY, PRICE, CURR) values
+(1,'2020-06-14 00:00:00','2020-12-31 23:59:59',1,35455,0,35.50,'EUR'),
+(1,'2020-06-14 15:00:00','2020-06-14 18:30:00',2,35455,1,25.45,'EUR'),
+(1,'2020-06-15 00:00:00','2020-06-15 11:00:00',3,35455,1,30.50,'EUR'),
+(1,'2020-06-15 16:00:00','2020-12-31 23:59:59',4,35455,1,38.95,'EUR');
